183546d9dbe4da7d86e30c259c0712a61295b839
liamgui <will@fai2.com>
2022-11-21 15:30:15 -1000

    filter component and some initial filtering

---
+++ src/components/Filters.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Dropdown from "~/components/Dropdown.vue";
+import { usePokemonTypesQuery } from "~/composables/usePokemon";
+import { Ref, ref, computed } from "vue";
+// pokemon types query
+const { types, loading: pokemonTypesLoading, error: pokemonTypesError } = usePokemonTypesQuery();
+
+
+</script>
+<template>
+	<Dropdown :options="types"></Dropdown>
+</template>
