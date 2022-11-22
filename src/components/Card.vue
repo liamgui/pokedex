507d2429174ecc6697091505e485c181a3f8a768
liamgui <will@fai2.com>
2022-11-21 19:02:43 -1000

    componentizing in prep for filters

---
+++ src/components/Card.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useFavorites } from "~/composables/useFavorites";
+import { Pokemon } from "~/types";
+import HeartSVG from "~/assets/svgs/heart.svg?component";
+import HeartFilledSVG from "~/assets/svgs/heart-filled.svg?component";
+import Audio from "~/components/Audio.vue";
+
+interface Props {
+	pokemon: Pokemon,
+}
+defineProps<Props>();
+
+// favorites composable
+const { toggleFavorite } = useFavorites();
+
+</script>
+<template>
+	<div class="card">
+		<router-link :to="'/pokemon/' + pokemon.name.replace(' ', '_').toLowerCase()">
+			<img v-if="pokemon.image" :src="pokemon.image" />
+		</router-link>
+		<div class="bottom-bar">
+			<router-link :to="'/pokemon/' + pokemon.name.replace(' ', '_').toLowerCase()">
+				<h2>{{ pokemon.name }}</h2>
+			</router-link>
+			<div v-if="pokemon.sound">
+				<Audio :sound-src="pokemon.sound" />
+			</div>
+			{{ pokemon.types.join(", ") }}
+			<button class="heart-button" @click="toggleFavorite(pokemon)">
+				<HeartSVG class="empty" />
+				<transition name="heart">
+					<HeartFilledSVG v-if="pokemon.isFavorite" class="filled" />
+				</transition>
+			</button>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.card {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	background: white;
+	/* width: 310px; */
+	box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+	& img {
+		height: 200px;
+	}
+	& h2 {
+		color: var(--black);
+	}
+}
+
+.heart-button {
+	background: transparent;
+	border: none;
+	position: relative;
+    height: 80px;
+	& svg {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+	&:focus {
+		outline: none;
+	}
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,9 +3,8 @@
 import { computed, ref, Ref } from "vue";
 //head
 import { useHead } from "@vueuse/head";
-//svgs
-import HeartSVG from "~/assets/svgs/heart.svg?component";
-import HeartFilledSVG from "~/assets/svgs/heart-filled.svg?component";
+//card
+import Card from "~/components/Card.vue";
 //types
 import { Pokemon } from "~/types";
 //composables
@@ -26,13 +25,12 @@ useHead({
 });
 
 // pokemon query
-const { pokemons, offset, count, loading: pokemonsLoading, error: pokemonsError } = usePokemonQuery();
+const { pokemons, count, loading: pokemonsLoading, error: pokemonsError, loadMore } = usePokemonQuery();
 
-// sounds object for easy retrieval
-const sounds: Ref<Record<string, HTMLAudioElement>> = ref({});
+const load = () => {
+	loadMore();
+};
 
-// favorites composable
-const { toggleFavorite } = useFavorites();
 
 </script>
 
@@ -40,29 +38,9 @@ const { toggleFavorite } = useFavorites();
 	<div class="content">
 		<!-- <Filters></Filters> -->
 		<div v-if="!pokemonsLoading && !pokemonsError">
-			<ul v-if="pokemons && pokemons.length" class="pokemon-list">
+			<ul v-if="pokemons && pokemons.length" v-infinite-scroll="load" class="pokemon-list">
 				<li v-for="pokemon in pokemons" :key="pokemon.id" :class="{favorite: pokemon.isFavorite}">
-					<div class="card">
-						<router-link :to="'/pokemon/' + pokemon.name.replace(' ', '_').toLowerCase()">
-							<img v-if="pokemon.image" :src="pokemon.image" />
-						</router-link>
-						<div class="bottom-bar">
-							<router-link :to="'/pokemon/' + pokemon.name.replace(' ', '_').toLowerCase()">
-								<h2>{{ pokemon.name }}</h2>
-							</router-link>
-							<div v-if="pokemon.sound">
-								<audio :id="'sounds' + pokemon.id" :ref="(el) => {sounds[pokemon.id] = el as HTMLAudioElement}" :src="pokemon.sound.replace('/\./g', '').replace(' ', '')"></audio>
-								<button @click="sounds[pokemon.id].play()"></button>
-							</div>
-							{{ pokemon.types.join(", ") }}
-							<button class="heart-button" @click="toggleFavorite(pokemon)">
-								<HeartSVG class="empty" />
-								<transition name="heart">
-									<HeartFilledSVG v-if="pokemon.isFavorite" class="filled" />
-								</transition>
-							</button>
-						</div>
-					</div>
+					<Card :pokemon="pokemon" />
 				</li>
 			</ul>
 		</div>
@@ -83,38 +61,5 @@ const { toggleFavorite } = useFavorites();
 	grid-template-columns: repeat(auto-fill, 400px);
 	grid-gap: 20px;
 }
-
-.card {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	/* width: 310px; */
-	box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-	& img {
-		height: 200px;
-	}
-	& h2 {
-		color: var(--black);
-	}
-}
-
-.heart-button {
-	background: transparent;
-	border: none;
-	position: relative;
-    height: 80px;
-	& svg {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-	}
-	&:focus {
-		outline: none;
-	}
-}
-
 
 </style>
