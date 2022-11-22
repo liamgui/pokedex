93371fbe616f9369178a698b228d6fb9c8bdcec3
liamgui <will@fai2.com>
2022-11-21 19:02:43 -1000

    componentizing in prep for filters

---
+++ src/components/Audio.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+interface Props {
+	soundSrc: string,
+}
+defineProps<Props>();
+
+//audio element ref
+const audio = ref<HTMLAudioElement | null>(null);
+</script>
+
+<template>
+	<audio ref="audio" :src="soundSrc"></audio>
+	<button @click="audio?.play()"></button>
+</template>
