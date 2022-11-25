9641084d17524a4d5ae59cf96ba3501b2c24d893
liamgui <will@fai2.com>
2022-11-24 23:19:01 -0500

    Lato to fira sans font

--- vite.config.ts
+++ vite.config.ts
@@ -18,8 +18,8 @@ export default defineConfig({
 			google: {
 				families: [
 					{
-						name: "Lato",
-						styles: "wght@300;400;700;900",
+						name: "Fira Sans",
+						styles: "wght@300;400;700;800;900;",
 					}
 				],
 			},
