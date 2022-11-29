02eee2d1c59b460d463dae1a63fc5d43ccbc3374
liamgui <will@fai2.com>
2022-11-29 00:07:20 -0500

    added styles and svg to audio component

--- src/components/Audio.vue
+++ src/components/Audio.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import SoundSVG from "~/assets/svgs/sound.svg?component";
 
 interface Props {
 	soundSrc: string,
@@ -12,5 +13,38 @@ const audio = ref<HTMLAudioElement | null>(null);
 
 <template>
 	<audio ref="audio" :src="soundSrc"></audio>
-	<button @click="audio?.play()"></button>
+	<button @click="audio?.play()">
+		<SoundSVG></SoundSVG>
+	</button>
 </template>
+
+<style scoped>
+
+button {
+	background: transparent;
+	border: none;
+	position: relative;
+    /* height: 50px; */
+	height: 100%;
+	width: 100%;
+	&:deep() svg * {
+		transition: all 0.2s ease-in-out;
+	}
+	&:hover:deep() {
+		& svg * {
+			filter: brightness(0.75);
+		}
+	}
+	& svg {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+	&:focus {
+		outline: none;
+	}
+}
+
+</style>
