81c9c7218996ffe266740bc32e130977a606fc45
liamgui <will@fai2.com>
2022-11-29 00:11:05 -0500

    added loader and adjusted layout and styles added error message

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-import { computed } from "vue";
+import { computed, ref } from "vue";
 //head
 import { useHead } from "@vueuse/head";
 import { usePokemonQuery } from "~/composables/usePokemon";
@@ -8,9 +8,9 @@ import { useFilterStore } from "~/stores/useFilterStore";
 
 import Filters from "~/components/Filters.vue";
 import PokemonList from "~/components/PokemonList.vue";
+import Loader from "~/components/Loader.vue";
 import { useViewStore } from "~/stores/useViewStore";
 import { storeToRefs } from "pinia";
-
 //define head
 useHead({
 	title: "Pokedex",
@@ -23,18 +23,19 @@ useHead({
 });
 
 const { pokemons, loading: pokemonsLoading, error: pokemonsError, loadMore, count } = usePokemonQuery();
-
 const { viewType: viewTypeRef } = storeToRefs(useViewStore());
 
+const loadingMore = ref(false);
 
 const load = () => {
 	// if pokemon count is greater than or equal to total count.. don't load more
 	// or if pokemons count is 0, don't load more
 	if (!pokemons.value.length || pokemons.value.length >= count.value) return;
-
-	//need a debounce here?
-	setTimeout(() => {
-		loadMore();
+	loadingMore.value = true;
+	//need a debounce here to prevent multiple calls
+	setTimeout(async () => {
+		await loadMore();
+		loadingMore.value = false;
 	}, 50);
 };
 
@@ -52,16 +53,17 @@ const filteredPokemons = computed(() => {
 	<div class="content">
 		<Filters></Filters>
 		<div class="auto-scroll">
-			<div
-				v-if="!pokemonsLoading && !pokemonsError"
-				v-infinite-scroll="load"
-			>
+			<div v-infinite-scroll="load" infinite-scroll-distance="20">
 				<div v-if="filteredPokemons && filteredPokemons.length">
 					<PokemonList :pokemons="filteredPokemons" :view-type="viewTypeRef"></PokemonList>
 				</div>
 			</div>
-			<div v-else-if="pokemonsError"></div>
-			<div v-else class="loading"></div>
+		</div>
+		<div v-if="pokemonsError" class="error">
+			<p>There was an error loading the pokemons. Please refresh the page and try again.</p>
+		</div>
+		<div v-if="pokemonsLoading || loadingMore" class="loading" :class="{more: loadingMore}">
+			<Loader></Loader>
 		</div>
 	</div>
 </template>
@@ -79,6 +81,17 @@ const filteredPokemons = computed(() => {
 .auto-scroll {
 	overflow-y: auto;
 	height: 100%;
+}
+
+.loading {
+	margin: 2rem auto;
+	width: 100%;
+	&.more {
+		position: absolute;
+		left: 0;
+		bottom: 3rem;
+		z-index: 2;
+	}
 }
 
 </style>
