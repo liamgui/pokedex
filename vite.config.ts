3e83c1e75d8574465d0677cf1a01989c64bfe01a
liamgui <will@fai2.com>
2022-12-06 12:03:28 -0500

    added externals for all modules to decrease package size

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 /// <reference types="vitest" />
 
+
 import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import Pages from "vite-plugin-pages";
 import { VitePluginFonts } from "vite-plugin-fonts";
 import svgLoader from "vite-svg-loader";
+
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+const pkg = require("./package.json");
 
 export default defineConfig({
 	resolve: {
@@ -30,6 +35,27 @@ export default defineConfig({
 		}),
 		svgLoader()
 	],
+	build: {
+		rollupOptions: {
+			// make sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: Object.keys(pkg.dependencies),
+			// output: {
+			// 	// Provide global variables to use in the UMD build
+			// 	// for externalized deps
+			// 	globals: {
+			// 		vue: "Vue",
+			// 		"vue-router": "VueRouter",
+			// 		pinia: "Pinia",
+			// 		vite: "Vite",
+			// 		"vite-plugin-pages": "VitePluginPages",
+			// 		"vite-plugin-fonts": "VitePluginFonts",
+			// 		"vite-svg-loader": "ViteSvgLoader",
+
+			// 	}
+			// }
+		}
+	},
 	test: {
 		globals: true,
 		environment: "jsdom"
